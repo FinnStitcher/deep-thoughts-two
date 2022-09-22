505da2f6a6e943c3392a2178f776306b008fe2ab
Finn Stitcher <foundfootage@protonmail.com>
2022-09-22 13:25:34 -0700

    single thought

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
-    const {id: thoughtId} = useParams();
-    console.log(thoughtId);
-    
-  return (
-    <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
-        </p>
-        <div className="card-body">
-          <p>Thought Text</p>
-        </div>
-      </div>
-    </div>
-  );
+	const { id: thoughtId } = useParams();
+	const { loading, data } = useQuery(QUERY_THOUGHT, {
+		variables: { id: thoughtId }
+	});
+	// passing object in as the variable for the query
+
+	const thought = data?.thought || {};
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div>
+			<div className="card mb-3">
+				<p className="card-header">
+					On {thought.createdAt},{' '}
+					<Link
+						to={`/profile/${thought.username}`}
+						style={{ fontWeight: 700 }}
+						className="text-light"
+					>
+						{thought.username}
+					</Link>{' '}
+					thought...
+				</p>
+				<div className="card-body">
+					<p>{thought.thoughtText}</p>
+				</div>
+			</div>
+			{thought.reactionCount > 0 && (
+				<ReactionList reactions={thought.reactions} />
+			)}
+		</div>
+	);
 };
 
 export default SingleThought;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,4 +16,22 @@ query thoughts($username: String) {
         }
     }
 }
-`
+`;
+
+export const QUERY_THOUGHT = gql`
+query thought($id: ID!) {
+    thought(_id: $id) {
+        _id
+        thoughtText
+        createdAt
+        username
+        reactionCount
+        reactions {
+            _id
+            createdAt
+            username
+            reactionBody
+        }
+    }
+}
+`;
